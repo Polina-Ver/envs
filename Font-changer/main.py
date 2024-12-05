206a53fb98fc1a5e3cedf876f8a04c6852ed6f66
Polina-Ver <vershpol1987@hmail.com>
2024-12-05 11:59:10 +0500

    добавили импорты в main

---
+++ Font-changer/main.py
@@ -0,0 +1,13 @@
+## main стартовый модуль проекта
+
+#import <зависимость из задания>
+
+from functions import func1, func2
+
+def main():
+    pass
+
+# инициализационный скрипт
+
+if _name_ == "_main_":
+    main() 
